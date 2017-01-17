7f3fbdd6dfba9f2655733d3045fdcf6b9288c32e
Alp Deveci <iceveda@gmail.com>
2017-01-17 15:00:19 -0600

    Random Quote Generator w/o twitter share

---
+++ js/app.js
@@ -0,0 +1,18 @@
+/* using .ajax method */
+
+
+
+
+
+/* using .getJSON method */
+
+  $(document).ready(function() {
+    $("#getMessage").on("click", function() {
+ 		$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
+   			 var quote = json.quoteText;
+   			 var author = json.quoteAuthor;
+   			$("#quoteText").html(quote);
+   			$("#author").html(author);
+ 		});
+    });
+  });
